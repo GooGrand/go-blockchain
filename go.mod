8da87daaa816abc1a50ae19adf8b8df8c470e44f
GooGrand <55867268+GooGrand@users.noreply.github.com>
2020-12-12 16:04:33 +0600

    initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/GooGrand/go-blockchain.git
+
+go 1.13
+
+require rsc.io/quote v1.5.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com//GooGrand/go-blockchain/blockchain"
+)
+
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
